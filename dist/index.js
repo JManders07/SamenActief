a5cde6103da6f933a33e12d987b912cb90ac9000
Wessel <129429254+Wepsel@users.noreply.github.com>
2025-04-14 09:23:26 +0200

    SEO toevoeging (Robots.txt en Sitemap.xml)

--- dist/index.js
+++ dist/index.js
@@ -1379,7 +1379,7 @@ async function setupVite(app2, server) {
   });
 }
 function serveStatic(app2) {
-  const distPath = path4.resolve(__dirname3, "public");
+  const distPath = path4.resolve(__dirname3, "..", "dist", "public");
   if (!fs.existsSync(distPath)) {
     throw new Error(
       `Could not find the build directory: ${distPath}, make sure to build the client first`
