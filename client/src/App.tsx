8fbfdd62ebdf46f23a92b05c6a2ff788267b5a0a
Wessel <129429254+Wepsel@users.noreply.github.com>
2025-05-12 10:36:11 +0200

    Goede router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Switch, Route } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
@@ -22,24 +22,22 @@ import ResetPassword from '@/pages/ResetPassword';
 
 function Router() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Index />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/centers/:id" element={<Center />} />
-        <Route path="/activities/:id" element={<Activity />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/auth" element={<Auth />} />
-        <Route path="/reset-password" element={<ResetPasswordPage />} />
-        <Route path="/reset-password/:token" element={<ResetPassword />} />
-        <Route path="/center-admin" element={<CenterAdmin />} />
-        <Route path="/activity-stats" element={<ActivityStats />} />
-        <Route path="/help" element={<Help />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/about" element={<About />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </BrowserRouter>
+    <Switch>
+      <Route path="/" component={Index} />
+      <Route path="/home" component={Home} />
+      <Route path="/centers/:id" component={Center} />
+      <Route path="/activities/:id" component={Activity} />
+      <Route path="/profile" component={Profile} />
+      <Route path="/auth" component={Auth} />
+      <Route path="/reset-password" component={ResetPasswordPage} />
+      <Route path="/reset-password/:token" component={ResetPassword} />
+      <Route path="/center-admin" component={CenterAdmin} />
+      <Route path="/activity-stats" component={ActivityStats} />
+      <Route path="/help" component={Help} />
+      <Route path="/contact" component={Contact} />
+      <Route path="/about" component={About} />
+      <Route component={NotFound} />
+    </Switch>
   );
 }
 
